7fd1de1dbe49ad213aa992aac792aa45bf54364d
Edgar Neverovski <edgarneverovskij@gmail.com>
2025-03-12 00:37:34 +0200

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CourierApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private readonly ServiceProvider _serviceProvider;
+
+    public App()
+    {
+        var services = new ServiceCollection();
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlite("Data Source=mydatabase.db"));
+        _serviceProvider = services.BuildServiceProvider();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        using var scope = _serviceProvider.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        context.Database.EnsureCreated();
+    }
+}
+
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            // Указываем путь к файлу базы данных SQLite
+            optionsBuilder.UseSqlite("Data Source=mydatabase.db");
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CourierApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly AppDbContext _context;
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public MainWindow(AppDbContext context)
+    {
+        InitializeComponent();
+        _context = context;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var user = new User { Name = "Jane Doe", Age = 25 };
+        _context.Users.Add(user);
+        _context.SaveChanges();
+    }
+}
